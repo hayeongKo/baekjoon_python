1299fb87509009d584223f067deaf9f412334df8
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2024-10-09 15:30:35 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -14,26 +14,13 @@ public class Main {
             p[i] = Integer.parseInt(inputs[i-1]);
         }
 
-        int[][] dp = new int[N+1][N+1]; // 가로: 카드 개수 | 세로: 구매가능 카드팩 인덱스
-        for(int i = 1; i <= N; i++) {
-            dp[1][i] = dp[1][i-1] + p[1];
-        }
+        int[] dp = new int[N+1];
 
-        for(int i = 2; i <= N; i++) { // 구매가능한 카드팩 인덱스 (카드팩 내 갯수)
-            for(int j = 1; j <= N; j++) { // 총 카드 개수
-                if(i > j) {
-                    dp[i][j] = dp[i-1][j];
-                } if(i == j) {
-                    dp[i][j] = Math.max(p[j], dp[i-1][j]);
-                } else {
-                    int maxNum = dp[i-1][j];
-                    for(int k = j-1; k >= j/2; k--) {
-                        maxNum = Math.max(maxNum, dp[i][k]+dp[i][j-k]);
-                    }
-                    dp[i][j] = maxNum;
-                }
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j]+p[j]);
             }
         }
-        System.out.println(dp[N][N]);
+        System.out.println(dp[N]);
     }
 }
