127d38d6997b12addc507cfe719db468756f86a4
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-03-05 18:43:05 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 780 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,49 +1,37 @@
-// 17413
-// 단어 뒤집기 2
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split("");
+        String input = br.readLine();
 
-        // for (String i : input) {
-        //     System.out.println(i);
-        // }
+        boolean tagFlag = false;
+        ArrayDeque<Character> q = new ArrayDeque<>();
 
-        List<String> tag = new ArrayList<>();
-        Deque<String> temp = new ArrayDeque<>();
-        boolean flag = false;
-
-        for (String spell : input) {
-            if (spell.equals("<")) {
-                while (!temp.isEmpty()) {
-                    System.out.print(temp.pollLast());
-                }
-                tag.add(spell);
-                flag = true;
-            } else if (spell.equals(">")) {
-                tag.add(spell);
-                System.out.print(String.join("", tag)); // ''.join(list) 랑 똑같은거
-                tag.clear();
-                flag = false;
-            } else if (flag) {
-                tag.add(spell);
-            } else if (spell.equals(" ")) {
-                while (!temp.isEmpty()) {
-                    System.out.print(temp.pollLast());
-                }
-                System.out.print(" ");
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '<') {
+                while(!q.isEmpty()) System.out.print(q.pollLast());
+                tagFlag = true;
+                System.out.print(c);
+            } else if (c == '>') {
+                tagFlag = false;
+                System.out.print(c);
             } else {
-                temp.add(spell);
+                if (tagFlag) System.out.print(c);
+                else {
+                    if (c == ' ') {
+                        while(!q.isEmpty()) System.out.print(q.pollLast());
+                        System.out.print(" ");
+                    } else {
+                        q.offer(c);
+                    }
+                }
             }
+            
         }
 
-        while (!temp.isEmpty()) {
-            System.out.print(temp.pollLast());
-        }
-
+        while (!q.isEmpty()) System.out.print(q.pollLast());
     }
 }
