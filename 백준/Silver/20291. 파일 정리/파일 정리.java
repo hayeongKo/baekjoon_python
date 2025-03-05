ed52da5192826f5760cf9b8d69efe43a92cf8947
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-03-05 19:03:14 +0900

    [Silver III] Title: 파일 정리, Time: 1092 ms, Memory: 61000 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,31 +1,28 @@
-// 20291
-// 파일 정리
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         HashMap<String, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            String[] inputs = br.readLine().split("\\.");
-
-            if (map.containsKey(inputs[1])) {
-                map.replace(inputs[1], map.get(inputs[1]) + 1);
+        String[] inputs;
+        for(int i = 0; i < N; i++) {
+            inputs = br.readLine().split("\\.");
+            String file = inputs[1];
+            if (map.containsKey(file)) {
+                map.replace(file, map.get(file)+1);
             } else {
-                map.put(inputs[1], 1);
+                map.put(file, 1);
             }
         }
 
         List<String> keySet = new ArrayList<>(map.keySet());
-
         Collections.sort(keySet);
 
-        for (String key : keySet) {
+        for(String key : keySet) {
             System.out.println(key + " " + map.get(key));
         }
     }
