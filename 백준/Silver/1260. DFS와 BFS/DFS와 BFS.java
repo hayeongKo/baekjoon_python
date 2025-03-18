4bc66e42ce827d1cfe7224aa034575da425434ce
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-03-18 19:38:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 300 ms, Memory: 22604 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,64 +1,59 @@
-// 1260
-// DFS와 BFS
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     static int N, M, V;
-    static int[][] map;
-    static boolean[] visitedDfs;
-    static boolean[] visitedBfs;
-    static ArrayDeque<Integer> queue = new ArrayDeque<>();
-    
+    static boolean[][] graph;
+    static boolean[] visitedDFS;
+    static boolean[] visitedBFS;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
-
         N = Integer.parseInt(inputs[0]);
         M = Integer.parseInt(inputs[1]);
         V = Integer.parseInt(inputs[2]);
+        
+        graph = new boolean[N+1][N+1];
+        visitedDFS = new boolean[N+1];
+        visitedBFS = new boolean[N+1];
 
-        map = new int[N+1][N+1];
-
+        int s = 0, e = 0;
         for(int i = 0; i < M; i++) {
             inputs = br.readLine().split(" ");
-            map[Integer.parseInt(inputs[0])][Integer.parseInt(inputs[1])] = map[Integer.parseInt(inputs[1])][Integer.parseInt(inputs[0])] = 1;
+            s = Integer.parseInt(inputs[0]);
+            e = Integer.parseInt(inputs[1]);
+            graph[s][e] = graph[e][s] = true;
         }
-
-        visitedDfs = new boolean[N+1];
-        visitedBfs = new boolean[N+1];
         dfs(V);
         System.out.println();
         bfs(V);
     }
 
     public static void dfs(int v) {
-        System.out.print(v+" ");
-        visitedDfs[v] = true;
-
-        for(int j = 0; j<= N; j++) {
-            if (map[v][j] == 1 && !visitedDfs[j]) {
-                dfs(j);
+        System.out.print(v + " ");
+        visitedDFS[v] = true;
+        
+        for(int i = 1; i <= N; i++) {
+            if (graph[v][i] && !visitedDFS[i]) {
+                dfs(i);
             }
         }
     }
 
     public static void bfs(int v) {
-        System.out.print(v+" ");
-        visitedBfs[v] = true;
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        visitedBFS[v] = true;
         queue.offer(v);
 
-        while (!queue.isEmpty()) {
-            int temp = queue.poll();
-            for(int j = 0; j <= N; j++) {
-                if (map[temp][j] == 1 && !visitedBfs[j]) {
-                    visitedBfs[j] = true;
-                    System.out.print(j+" ");
-                    queue.offer(j);
+        while(!queue.isEmpty()) {
+            int now = queue.pop();
+            System.out.print(now+" ");
+            for(int i = 1; i <= N; i++) {
+                if(graph[now][i] && !visitedBFS[i]) {
+                    visitedBFS[i] = true;
+                    queue.offer(i);
                 }
             }
         }
-        
     }
 }
