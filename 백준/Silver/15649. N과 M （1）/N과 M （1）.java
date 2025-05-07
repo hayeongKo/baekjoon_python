bc23c5cd206c9f3b2a8f61a0b2bfd23a8affc4f0
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-05-07 17:09:10 +0900

    [Silver III] Title: N과 M (1), Time: 2256 ms, Memory: 89428 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]); // 1~N 사이
+        M = Integer.parseInt(inputs[1]); // 길이
+
+        visited = new boolean[N+1];
+        dfs(0, new ArrayList<>());
+
+    }
+
+    public static void dfs(int depth, List<Integer> list) {
+        if (depth == M) {
+            for(Integer i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(i);
+                dfs(depth+1, list);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
+        }
+    }
+}
