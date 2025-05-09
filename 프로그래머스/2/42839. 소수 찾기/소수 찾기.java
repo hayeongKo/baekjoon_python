0f027b12c42c0a77fa0986b1213772f1aa1d2ea8
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-05-09 17:19:05 +0900

    [level 2] Title: 소수 찾기, Time: 51.38 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,19 +1,16 @@
 import java.util.*;
 
 class Solution {
-    int N;
     HashSet<Integer> numberSet;
-    
     public int solution(String numbers) {
-        N = numbers.length();
         numberSet = new HashSet<>();
-        
         permute("", numbers);
         
         int ans = 0;
         for(int num : numberSet) {
             if (isPrime(num)) ans++;
         }
+        
         return ans;
     }
     
@@ -29,16 +26,15 @@ class Solution {
         }
     }
     
-    
-    
     public boolean isPrime(int n) {
-        if (n <= 1) return false;
+        if (n == 1) return false;
         if (n == 2) return true;
-        if (n%2==0) return false;
+        if (n % 2 == 0) return false;
         
         for(int i = 3; i <= Math.sqrt(n); i++) {
-            if (n%i == 0) return false;
+            if (n%i==0) return false;
         }
+        
         return true;
     }
 }
