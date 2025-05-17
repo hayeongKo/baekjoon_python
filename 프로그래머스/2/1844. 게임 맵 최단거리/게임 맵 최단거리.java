1837a77fb5f378effcaf9ec313baf95917a4c812
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-05-17 09:07:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.44 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 class Solution {
+    static boolean[][] visited;
     static int N, M;
+    
     class Point {
         int x, y, d;
         Point(int x, int y, int d) {
@@ -10,17 +12,16 @@ class Solution {
             this.d = d;
         }
     }
+    
     public int solution(int[][] maps) {
         N = maps.length;
         M = maps[0].length;
-        
+        visited = new boolean[N][M];
         return bfs(maps);
     }
     
     public int bfs(int[][] maps) {
         ArrayDeque<Point> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][M];
-        
         queue.offer(new Point(0, 0, 1));
         visited[0][0] = true;
         
@@ -28,20 +29,18 @@ class Solution {
         int[] dy = {1, -1, 0, 0};
         
         while(!queue.isEmpty()) {
-            Point now = queue.pop();
-            if (now.x == N-1 && now.y == M-1) return now.d; // 끝에 도달
+            Point now = queue.poll();
+            if (now.x == N-1 && now.y == M-1) return now.d;
             
             for(int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
                 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue; // 맵 벗어나는 경우
-                if (maps[nx][ny] == 0) continue; // 벽과 마주치는 경우
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (visited[nx][ny] || maps[nx][ny] == 0) continue;
                 
-                if (!visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    queue.offer(new Point(nx, ny, now.d+1));
-                }
+                visited[nx][ny] = true;
+                queue.offer(new Point(nx, ny, now.d+1));
             }
         }
         
