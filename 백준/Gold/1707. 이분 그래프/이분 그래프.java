e169657d0b6e2ee0c438be541a8d8ecaf4248544
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-05-17 20:47:17 +0900

    [Gold IV] Title: 이분 그래프, Time: 992 ms, Memory: 306212 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,48 +1,43 @@
-// 백준 1707
-// 이분 그래프
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int K, V, E; 
-    static Map<Integer, List<Integer>> graph;
+    static int K, V, E;
+    static List<Integer>[] graph;
     static int[] color;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         K = Integer.parseInt(br.readLine());
-        
+        String[] inputs;
+
         while(K-- > 0) {
-            String[] inputs = br.readLine().split(" ");
+            inputs = br.readLine().split(" ");
             V = Integer.parseInt(inputs[0]);
             E = Integer.parseInt(inputs[1]);
-            graph = new HashMap<>();
+            graph = new ArrayList[V+1];
             color = new int[V+1];
 
-            for(int i = 1; i <= V; i++) {
-                graph.put(i, new ArrayList<>());
-            }
+            for(int i = 1; i <= V; i++) graph[i] = new ArrayList<>();
 
             for(int i = 0; i < E; i++) {
                 inputs = br.readLine().split(" ");
                 int s = Integer.parseInt(inputs[0]);
                 int e = Integer.parseInt(inputs[1]);
-
-                graph.get(s).add(e);
-                graph.get(e).add(s);
+                graph[s].add(e);
+                graph[e].add(s);
             }
 
-            boolean isBipartite = true;
+            boolean isBipart = true;
             for(int i = 1; i <= V; i++) {
                 if (color[i] == 0) {
-                    if(!bfs(i)) {
-                        isBipartite = false;
+                    if (!bfs(i)) {
+                        isBipart = false;
                         break;
                     }
                 }
             }
 
-            System.out.println(isBipartite ? "YES" : "NO");
+            System.out.println(isBipart ? "YES" : "NO");
         }
     }
 
@@ -52,18 +47,16 @@ public class Main {
         color[start] = 1;
 
         while(!queue.isEmpty()) {
-            int node = queue.poll();
+            int now = queue.poll();
 
-            for(int adj : graph.get(node)) {
+            for(int adj : graph[now]) {
                 if (color[adj] == 0) {
-                    color[adj] = -color[node];
+                    color[adj] = -color[now];
                     queue.offer(adj);
-                } else if(color[adj] == color[node]) {
-                    return false;
-                }
+                } else if (color[adj] == color[now]) return false;
             }
         }
-
+        
         return true;
     }
 }
