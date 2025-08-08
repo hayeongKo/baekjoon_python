c5a41e1e5ef71bd46bff3e22f5c83f2655e4a68d
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-08-08 13:23:13 +0900

    [level 3] Title: 봉인된 주문, Time: 229.54 ms, Memory: 151 MB -BaekjoonHub

---
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public String solution(long n, String[] bans) {
+        String answer = "";
+        long num;
+        long[] bansNums = new long[bans.length];
+        for(int i = 0; i < bans.length; i++) {
+            bansNums[i] = strToNum(bans[i]); // 26진수 -> 10진수 변환
+        }
+        
+        Arrays.sort(bansNums);
+        
+        for(long bn : bansNums) if(bn <= n) n++;
+        answer = numToStr(n);
+        
+        return answer;
+    }
+    
+    private long strToNum(String str) {
+        int length = str.length();
+        long res = 0;
+        for(int i = 0; i < length; i++) {
+            res += (str.charAt(i)-96)*Math.pow(26, (length-1-i));
+        }
+        return res;
+    }
+    
+    private String numToStr(long num) {
+        String str = "";
+        while(num > 0) {
+            str = String.valueOf((char)((num-1)%26+1+96)) + str;
+            num = (num-1)/26;
+        }
+        return str;
+    }
+}
