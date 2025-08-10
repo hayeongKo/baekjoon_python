0d938a35916764d1e5ccb743f01f0d5da637104a
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-08-10 13:18:31 +0900

    [Gold IV] Title: 이분 그래프, Time: 908 ms, Memory: 272208 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,62 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int K, V, E;
+    static int K;
     static List<Integer>[] graph;
     static int[] color;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         K = Integer.parseInt(br.readLine());
-        String[] inputs;
 
         while(K-- > 0) {
-            inputs = br.readLine().split(" ");
-            V = Integer.parseInt(inputs[0]);
-            E = Integer.parseInt(inputs[1]);
-            graph = new ArrayList[V+1];
-            color = new int[V+1];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph = new ArrayList[v+1];
+            color = new int[v+1];
 
-            for(int i = 1; i <= V; i++) graph[i] = new ArrayList<>();
+            for(int i = 1; i <= v; i++) graph[i] = new ArrayList<>();
 
-            for(int i = 0; i < E; i++) {
-                inputs = br.readLine().split(" ");
-                int s = Integer.parseInt(inputs[0]);
-                int e = Integer.parseInt(inputs[1]);
-                graph[s].add(e);
-                graph[e].add(s);
+            for(int i =0; i < e; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int d = Integer.parseInt(st.nextToken());
+                graph[s].add(d);
+                graph[d].add(s);
             }
 
-            boolean isBipart = true;
-            for(int i = 1; i <= V; i++) {
+            boolean flag = true;
+            for(int i = 1; i <= v; i++) {
                 if (color[i] == 0) {
                     if (!bfs(i)) {
-                        isBipart = false;
+                        flag = false;
                         break;
                     }
                 }
             }
 
-            System.out.println(isBipart ? "YES" : "NO");
+            System.out.println(flag ? "YES" : "NO");
         }
     }
 
     public static boolean bfs(int start) {
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        queue.offer(start);
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
         color[start] = 1;
 
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-
-            for(int adj : graph[now]) {
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for(int adj : graph[cur]) {
                 if (color[adj] == 0) {
-                    color[adj] = -color[now];
-                    queue.offer(adj);
-                } else if (color[adj] == color[now]) return false;
+                    color[adj] = -color[cur];
+                    q.offer(adj);
+                } else if (color[adj] == color[cur]) return false;
             }
         }
-        
+
         return true;
     }
 }
