403df331c83769fe722a23b27d308677f06c2a13
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-08-14 11:35:33 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 19200 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,75 +1,78 @@
-// 15686
-// 치킨배달 
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static class Pos {
-        int x, y;
-        Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static int N, M;
+	static int[][] map, dist;
+	static List<int[]> chickens;
+	static List<int[]> houses;
+	static long ans;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		chickens = new ArrayList<>();
+		houses = new ArrayList<>();
+		ans = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					houses.add(new int[]{i, j});
+				} else if (map[i][j] == 2) {
+					chickens.add(new int[]{i, j});
+				}
+			}
+		}
+		
+		dist = new int[houses.size()][chickens.size()];
+		
+		for(int h = 0; h < houses.size(); h++) {
+			for(int c = 0; c < chickens.size(); c++) {
+				int[] house = houses.get(h);
+				int[] chicken = chickens.get(c);
+				dist[h][c] = Math.abs(house[1]-chicken[1])+Math.abs(house[0]-chicken[0]);
+			}
+		}
+		
+//		for(int h = 0; h < houses.size(); h++) {
+//			System.out.println(Arrays.toString(dp[h]));
+//		}
+		
+		dfs(0, new ArrayList<>());
+		System.out.println(ans);
+	}
+	
+	public static void dfs(int start, List<Integer> visited) {
+		if (visited.size() == M) {
+			ans = Math.min(ans, countD(visited));
+			return;
+		}
+		
+		for(int i = start; i < chickens.size(); i++) {
+//			System.out.println(Arrays.toString(visited.toArray()));
+			visited.add(i);
+			dfs(i+1, visited);
+			visited.remove(visited.size()-1);
+		}
+	}
+	
+	public static long countD(List<Integer> visited) {
+		long d = 0;
+		for(int h = 0; h < houses.size(); h++) {
+			int t = Integer.MAX_VALUE;
+			for(int c : visited) {
+				t = Math.min(t, dist[h][c]);
+			}
+			d+=t;
+		}
+		return d;
+	}
 
-    static int N,M;
-    static int[][] city;
-    static List<Pos> chickens;
-    static List<Pos> houses;
-    static int res;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-
-        N = Integer.parseInt(inputs[0]);
-        M = Integer.parseInt(inputs[1]);
-        chickens = new ArrayList<>();
-        houses = new ArrayList<>();
-        city = new int[N][N];
-
-        // 초기화
-        for (int i = 0; i < N; i++) {
-            inputs = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                city[i][j] = Integer.parseInt(inputs[j]);
-                if (city[i][j] == 2) {
-                    chickens.add(new Pos(i, j));
-                } else if(city[i][j] == 1) {
-                    houses.add(new Pos(i, j));
-                }
-            }
-        }
-
-        visited = new boolean[chickens.size()];
-        res = Integer.MAX_VALUE;
-
-        dfs(0, 0);
-        System.out.println(res);
-    }
-
-    public static void dfs(int depth, int start) {
-        if (depth == M) {
-            int totalD = 0;
-            for (int i = 0; i < houses.size(); i++) {
-                int temp = Integer.MAX_VALUE;
-                for (int j = 0; j < chickens.size(); j++) {
-                    if (visited[j]) {
-                        int d = (int) Math.abs(houses.get(i).x-chickens.get(j).x) + Math.abs(houses.get(i).y-chickens.get(j).y);
-                        temp = Math.min(temp, d);
-                    }
-                }
-                totalD += temp;
-            }
-            res = Math.min(totalD, res);
-            return;
-        }
-
-        for (int i = start; i < chickens.size(); i++) {
-            visited[i] = true;
-            dfs(depth+1, i+1);
-            visited[i] = false;
-        }
-    }
 }
