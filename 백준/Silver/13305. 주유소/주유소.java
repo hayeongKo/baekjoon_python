0a06b7ef6fed374576507d418242ed9fc1dc9dc7
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-08-26 14:52:28 +0900

    [Silver III] Title: 주유소, Time: 312 ms, Memory: 30412 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dist = new int[N];
+		int[] cost = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N-1; i++) {
+			dist[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int ans = 0;
+		int step = 0;
+		int distance = 0;
+		int curCost = cost[0];
+
+		while(true) {
+			if (step == N-1) {
+				ans += distance*curCost;
+				break;
+			}
+			
+			if (curCost > cost[step]) {
+				ans += distance*curCost;
+				distance = 0;
+				curCost = cost[step];
+			} else {
+				distance += dist[step++];
+			}
+		}
+		
+		System.out.println(ans);
+
+	}
+
+}
