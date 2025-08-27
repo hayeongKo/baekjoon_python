9064175361c38043e301abf70604d6e8cfe9209b
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-08-27 09:11:24 +0900

    [Gold V] Title: 회의실 배정, Time: 460 ms, Memory: 44212 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,34 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        List<int[]> schedule = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            String[] inputs = br.readLine().split(" ");
-            int[] tmp = new int[2];
-            tmp[0] = Integer.parseInt(inputs[0]);
-            tmp[1] = Integer.parseInt(inputs[1]);
-            schedule.add(tmp);
-        }
 
-        schedule.sort((o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                return Integer.compare(o1[0], o2[0]);
-            }
-            return Integer.compare(o1[1], o2[1]);
-        });
-        
-        int answer = 0;
-        int end = 0;
-        for(int[] s : schedule) {
-            if (end <= s[0]) {
-                end = s[1];
-                answer++;
-            }
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] meeting = new int[N][2];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			meeting[i][0] = Integer.parseInt(st.nextToken());
+			meeting[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(meeting, (o1, o2) -> {
+			if (o1[1]==o2[1]) {
+				return o1[0]-o2[0];
+			}
+			return o1[1]-o2[1];
+		});
+		
+		int ans = 1;
+		int cur = 0;
+		for(int i = 1; i < N; i++) {
+			if (meeting[i][0] >= meeting[cur][1]) {
+				ans++;
+				cur=i;
+			}
+		}
+		
+		System.out.println(ans);
 
-        System.out.println(answer);
-    }
+	}
+
 }
