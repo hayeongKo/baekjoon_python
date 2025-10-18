17d93d4cd93858b718adb624d5ff907c74fee8a6
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-10-18 13:35:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.97 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visited;
-    static int N, M;
-    
-    class Point {
-        int x, y, d;
-        Point(int x, int y, int d) {
-            this.x = x;
-            this.y = y;
-            this.d = d;
-        }
-    }
-    
+    int N, M;
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
     public int solution(int[][] maps) {
         N = maps.length;
         M = maps[0].length;
-        visited = new boolean[N][M];
-        return bfs(maps);
-    }
-    
-    public int bfs(int[][] maps) {
-        ArrayDeque<Point> queue = new ArrayDeque<>();
-        queue.offer(new Point(0, 0, 1));
+        
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
+        q.offer(new int[]{0, 0, 1}); // x y d
         visited[0][0] = true;
         
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-        
-        while(!queue.isEmpty()) {
-            Point now = queue.poll();
-            if (now.x == N-1 && now.y == M-1) return now.d;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int d = cur[2];
+            
+            if (x == N-1 && y == M-1) {
+                return d;
+            }
             
             for(int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
+                int nx = x+dx[i];
+                int ny = y+dy[i];
                 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                if (visited[nx][ny] || maps[nx][ny] == 0) continue;
+                if (inRange(nx, ny) && !visited[nx][ny] && maps[nx][ny]!=0) {
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny, d+1});
+                }
                 
-                visited[nx][ny] = true;
-                queue.offer(new Point(nx, ny, now.d+1));
             }
         }
         
         return -1;
+    }
+    
+    public boolean inRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
