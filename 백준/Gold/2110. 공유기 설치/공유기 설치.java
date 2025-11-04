652c3dfe14ccc086c693bd2f29de7fcb86dd4dd0
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2025-11-04 11:07:43 +0900

    [Gold IV] Title: 공유기 설치, Time: 320 ms, Memory: 34280 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,59 +1,51 @@
-// 2110
-// 공유기 설치
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static ArrayList<Long> house;
-    static int C, N;
-
-
-    public static void main(String[] args) throws IOException {
+    static List<Integer> houses;
+    static int N, C;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-        N = Integer.parseInt(inputs[0]);
-        C = Integer.parseInt(inputs[1]);
-        house = new ArrayList<>();
+        houses = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            house.add(Long.parseLong(br.readLine()));
+        for(int i = 0; i < N; i++) {
+            houses.add(Integer.parseInt(br.readLine()));
         }
 
-        Collections.sort(house);
+        houses.sort(Comparator.naturalOrder());
 
-        long left = 1; // 가능한 최소 거리
-        long right = house.get(N-1)-house.get(0); // 가능한 최대 거리
+        long left = 1;
+        long right = houses.get(N-1);
+        long ans = 0;
 
-        long res = 0;
-        
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            // 공유기를 C개 만큼 설치 가능?
+        while(left <= right) {
+            long mid = (left+right)/2;
             if (check(mid)) {
-                // 설치 가능 -> 가장 인접한 두 공유기 사이의 거리를 최대로 해야함 -> 더 커져도 되는지 확인
-                res = Math.max(res, mid);
-                left = mid + 1;
+                ans = mid;
+                left = mid+1;
             } else {
-                // 설치 불가능 -> 현재 거리가 너무 멀다는 거니까 오른쪽 줄이기
-                right = mid - 1;
+                right = mid-1;
             }
         }
 
-        System.out.println(res);
+        System.out.println(ans);
     }
 
     public static boolean check(long dist) {
-        long cnt = 1;
-        long prev = house.get(0);
-
-        for (int i = 1; i < N; i++) {
-            if (house.get(i) - prev >= dist) { // 집 간의 거리가 설정한 최소 거리보다 크면 공유기 설치 가능
+        int cnt = 1;
+        int start = 0;
+        int next = 1;
+        while(next < N) {
+            if (houses.get(next)-houses.get(start) >= dist) {
                 cnt++;
-                prev = house.get(i);
+                if (cnt >= C) return true;;
+                start = next;
             }
+            next++;
         }
-
-        return cnt >= C;
+        return false;
     }
 }
